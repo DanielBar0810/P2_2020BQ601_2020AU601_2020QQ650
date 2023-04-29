9d00aac29a7f9cec49c23d1d8c5708884aca637c
Daniel <daniel.barrera@catolica.edu.sv>
2023-04-29 08:44:02 -0600

    Controllers

--- P2_2020BQ601_2020AU601_2020QQ650/Models/covidConnection.cs
+++ P2_2020BQ601_2020AU601_2020QQ650/Models/covidConnection.cs
@@ -9,8 +9,8 @@ namespace P2_2020BQ601_2020AU601_2020QQ650.Models
         {
         }
 
-        DbSet<CasosReportados> CasosReportados { get; set; }
-        DbSet<Departamentos> Departamentos { get; set; }
-        DbSet<Generos> Generos { get; set; }
+        public DbSet<CasosReportados> CasosReportados { get; set; }
+        public DbSet<Departamentos> Departamentos { get; set; }
+        public DbSet<Generos> Generos { get; set; }
     }
 }
