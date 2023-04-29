5fbd8911fd001638609625a4c0c015014c7062ce
Rodrigo Aguilar <rodrigo.aguilar@catolica.edu.sv>
2023-04-29 10:06:45 -0600

    .

--- P2_2020BQ601_2020AU601_2020QQ650/Controllers/CasosReportadosController.cs
+++ P2_2020BQ601_2020AU601_2020QQ650/Controllers/CasosReportadosController.cs
@@ -14,8 +14,8 @@ namespace P2_2020BQ601_2020AU601_2020QQ650.Controllers
             }
 
 
-            public IActionResult Index()
-            {
+        public IActionResult Index()
+        {
 
             var listadoDepartamentos = (from d in _covidConnection.Departamentos select d).ToList();
             //Manejador de controlador y vista
@@ -25,11 +25,32 @@ namespace P2_2020BQ601_2020AU601_2020QQ650.Controllers
 
             var listadoDeGeneros = (from g in _covidConnection.Generos select g).ToList();
             //Manejador de controlador y vista
-            ViewData["listadoDeGeneros"] = new SelectList(listadoDeGeneros, "id_genero","nombre_genero");
+            ViewData["listadoDeGeneros"] = new SelectList(listadoDeGeneros, "id_genero", "nombre_genero");
 
+            var listadoCasosReportados = (  from c in _covidConnection.CasosReportados
+                                            join d in _covidConnection.Departamentos
+                                            on c.id_departamento equals d.id_departamento
+                                            join g in _covidConnection.Generos
+                                            on c.id_genero equals g.id_genero
+                                            select new
+                                          {
+                                              departamento = d.nombre_departamento,
+                                              genero = g.nombre_genero,
+                                              confirmados = c.casosConfirmados,
+                                              recuperados = c.casosRecuperados,
+                                              fallecidos = c.casosFallecidos
+                                          }).ToList();
+            ViewData["listadoCasosReportados"] = listadoCasosReportados;
 
 
             return View();
-            }
+         }
+        public IActionResult crearRegistro(CasosReportados NuevoCaso) 
+        {
+            _covidConnection.Add(NuevoCaso);
+            _covidConnection.SaveChanges();
+            return RedirectToAction("Index");
+        
+        }
     }
 }
