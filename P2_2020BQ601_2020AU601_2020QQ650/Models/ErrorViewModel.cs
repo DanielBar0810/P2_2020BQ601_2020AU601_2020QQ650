94d2e51ecc1ecadad19f357991d1b602d9bcc635
Daniel <daniel.barrera@catolica.edu.sv>
2023-04-29 08:02:22 -0600

    Add project files.

---
+++ P2_2020BQ601_2020AU601_2020QQ650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020BQ601_2020AU601_2020QQ650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ P2_2020BQ601_2020AU601_2020QQ650/Models/covidConnection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace P2_2020BQ601_2020AU601_2020QQ650.Models
+{
+    public class covidConnection : DbContext
+    {
+        public covidConnection(DbContextOptions options) : base(options)
+        {
+        }
+
+        
+    }
+}
