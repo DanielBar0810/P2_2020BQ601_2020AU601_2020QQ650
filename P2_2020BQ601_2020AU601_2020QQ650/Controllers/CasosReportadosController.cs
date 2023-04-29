cca57cf2413fd2ec7b50a569fc755d102a6b1fc0
Rodrigo Aguilar <rodrigo.aguilar@catolica.edu.sv>
2023-04-29 09:03:24 -0600

    CasosReportados

---
+++ P2_2020BQ601_2020AU601_2020QQ650/Controllers/CasosReportadosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020BQ601_2020AU601_2020QQ650.Models;
+
+namespace P2_2020BQ601_2020AU601_2020QQ650.Controllers
+{
+    public class CasosReportadosController : Controller
+    {
+
+            private readonly covidConnection _covidConnection;
+            public CasosReportadosController(covidConnection covidConnection)
+            {
+                _covidConnection = covidConnection;
+            }
+
+
+            public IActionResult Index()
+            {
+
+            var listadoDepartamentos = (from d in _covidConnection.Departamentos select d).ToList();
+            //Manejador de controlador y vista
+            ViewData["listadoDepartamentos"] = new SelectList(listadoDepartamentos, "id_departamento", "nombre_departamento");
+
+
+
+
+            return View();
+            }
+    }
+}
