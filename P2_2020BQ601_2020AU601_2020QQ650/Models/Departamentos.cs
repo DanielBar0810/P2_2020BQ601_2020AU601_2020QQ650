089295becadf316e22a5a0dfe00e5a6581461eca
Luis Felipe Quezada Quezada <luisfeli12.lfq@gmail.com>
2023-04-29 08:28:03 -0600

    modelos

---
+++ P2_2020BQ601_2020AU601_2020QQ650/Models/Departamentos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_2020BQ601_2020AU601_2020QQ650.Models
+{
+    public class Departamentos
+    {
+        [Key]
+        public int id_departamento { get; set; }
+        public string nombre_departamento { get; set; }
+    }
+}
